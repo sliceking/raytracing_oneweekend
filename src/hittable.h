a7b9870f43ff14fa5dfcbd542165131e7479d1ee
stani <svwielga4@gmail.com>
2026-01-09 20:08:21 -0600

    adds camera and refactors, finished chapter 7

--- src/hittable.h
+++ src/hittable.h
@@ -1,6 +1,7 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "rtweekend.h"
 class hit_record {
 public:
   point3 p;
@@ -21,8 +22,7 @@ class hittable {
 public:
   virtual ~hittable() = default;
 
-  virtual bool hit(const ray &r, double ray_tmin, double ray_tmax,
-                   hit_record &rec) const = 0;
+  virtual bool hit(const ray &r, interval ray_t, hit_record &rec) const = 0;
 };
 
 #endif
